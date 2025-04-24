ed8b968759ce66a44494aca57838a3d2dcb9e3f6
admin <rifatkadriev@MacBook-Pro.Dlink>
2025-04-24 23:00:25 +0300

    создал проект

---
+++ src/test/java/edu/lcaitlyn/cloudfilestorage/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lcaitlyn.cloudfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
